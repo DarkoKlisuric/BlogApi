63c1185a8ba12f65b4ef399101bf8bbb12ff059b
DarkoKlisuric <klisuric1995@gmail.com>
2020-01-28 20:15:03 +0000

    First controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ * @package App\Controller
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default_index")
+     */
+    public function index()
+    {
+        return new JsonResponse([
+            'MyName' => 'Darko Klisurić'
+        ]);
+    }
+}
