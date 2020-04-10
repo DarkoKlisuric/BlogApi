54508492ff718362d19140d74d4e2b4b4d244566
dklisuric <klisuric1995@gmail.com>
2020-04-10 20:42:18 +0200

    Modifying access controls

--- src/Enum/RoleEnum.php
+++ src/Enum/RoleEnum.php
@@ -9,7 +9,7 @@ namespace App\Enum;
 class RoleEnum
 {
     const ROLE_COMMENTATOR = 'ROLE_COMMENTATOR';
-    const ROLE_WRITER = 'ROLE WRITER';
+    const ROLE_WRITER = 'ROLE_WRITER';
     const ROLE_EDITOR = 'ROLE_EDITOR';
     const ROLE_ADMIN = 'ROLE_ADMIN';
     const ROLE_SUPERADMIN = 'ROLE_SUPERADMIN';
