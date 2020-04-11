e736bf52b48380ee03ec31bbc9fd6177acd4e118
dklisuric <klisuric1995@gmail.com>
2020-04-11 11:32:30 +0200

    Automaticly change token when user changes his password

--- src/Controller/Action/ResetPasswordAction.php
+++ src/Controller/Action/ResetPasswordAction.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller\Action;
 
+use ApiPlatform\Core\Validator\ValidatorInterface;
 use App\Controller\AppController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class ResetPasswordAction
@@ -13,5 +19,66 @@ use App\Controller\AppController;
  */
 class ResetPasswordAction extends AppController
 {
+    /**
+     * @var ValidatorInterface
+     */
+    private ValidatorInterface $validator;
 
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $manager;
+
+    /**
+     * @var JWTTokenManagerInterface
+     */
+    private JWTTokenManagerInterface $tokenManager;
+
+    /**
+     * ResetPasswordAction constructor.
+     * @param ValidatorInterface $validator
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param EntityManagerInterface $manager
+     * @param JWTTokenManagerInterface $tokenManager
+     */
+    public function __construct(ValidatorInterface $validator,
+                                UserPasswordEncoderInterface $passwordEncoder,
+                                EntityManagerInterface $manager,
+                                JWTTokenManagerInterface $tokenManager)
+    {
+        $this->validator = $validator;
+        $this->passwordEncoder = $passwordEncoder;
+        $this->manager = $manager;
+        $this->tokenManager = $tokenManager;
+    }
+
+    /**
+     * @param User $data
+     *
+     * Validator is only called after we return the data from this action!
+     * Only hear is checks for user current password, but we've just modified it!
+     * @return JsonResponse
+     */
+    public function __invoke(User $data)
+    {
+        // Entity is persisted automatically, only if validaton pass
+        $this->validator->validate($data);
+
+        $data->setPassword(
+            $this->passwordEncoder->encodePassword(
+                $data, $data->getNewPassword()
+            )
+        );
+
+        $this->manager->flush();
+
+        $token = $this->tokenManager->create($data);
+
+        return new JsonResponse(['token' => $token]);
+    }
 }
