ade92e041bc6b36656631ed15e623391704ecb0f
dklisuric <klisuric1995@gmail.com>
2020-04-11 10:09:47 +0200

    Creating app controoler end reset password action class

---
+++ src/Controller/Action/ResetPasswordAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Action;
+
+use App\Controller\AppController;
+
+/**
+ * Class ResetPasswordAction
+ * @package App\Controller\Action
+ *
+ * In API platform for custom operations,
+ * we should create Action classes instead controller classes.
+ */
+class ResetPasswordAction extends AppController
+{
+
+}
