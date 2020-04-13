4060e5107077375c90b50bf0e156bd99afc3edd5
dklisuric <klisuric1995@gmail.com>
2020-04-13 19:45:10 +0200

    Creating first primitive logging message

--- src/Service/UserConfirmationService.php
+++ src/Service/UserConfirmationService.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use App\Exception\InvalidConfirmationTokenException;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class UserConfirmationService
 {
@@ -18,19 +19,30 @@ class UserConfirmationService
      */
     private EntityManagerInterface $manager;
 
-    public function __construct(UserRepository $userRepository, EntityManagerInterface $manager)
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    public function __construct(UserRepository $userRepository,
+                                EntityManagerInterface $manager,
+                                LoggerInterface $logger)
     {
         $this->userRepository = $userRepository;
         $this->manager = $manager;
+        $this->logger = $logger;
     }
 
     public function confirmUser(string $confirmationToken)
     {
+        $this->logger->debug('Fetching user by confirmation token');
+
         $user = $this->userRepository->findOneBy(
             ['confiramtationToken' => $confirmationToken]);
 
         // User was NOT found by confiration token
         if (!$user) {
+            $this->logger->debug('User by confirmation token not found');
             throw new InvalidConfirmationTokenException();
         }
 
@@ -38,5 +50,7 @@ class UserConfirmationService
         $user->setConfiramtationToken(null);
 
         $this->manager->flush();
+
+        $this->logger->debug('Confirmed user by confirmation token');
     }
 }
